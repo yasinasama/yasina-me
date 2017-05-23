f3e9ada80afd24f17f3afc2cef529d439e5faf6b
yasinasama <yasinasama01@gmail.com>
2017-05-23 09:53:00 +0800

    change staticfile

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import Http404
+from blog.models import Artical
 
 
-def getIndex(req):
+def getIndex(request):
+    artical = get_object_or_404(Artical, pk=1)
+    return render_to_response('../templates/blog/index.html', {'artical_id': artical.id, 'artical_title': artical.title})
 
-    return render(request=req, template_name='../templates/blog/index.html')
+
+def getArticalDetail(request, id):
+    artical = get_object_or_404(Artical, pk=id)
+    return render_to_response('../templates/blog/artical_detail.html', {'artical_id': artical.id, 'artical_title': artical.title})
+# def getIndex(request):
+#
+#     try:
+#         artical = Artical.objects.get(id=1)
+#
+#     except Artical.DoesNotExist:
+#         raise Http404()
+#
+#     return render_to_response('../templates/blog/index.html', {'artical': artical})
+#
+#
+# def page_not_found(request):
+#     return render_to_response('../templates/blog/404.html')
--- lm/urls.py
+++ lm/urls.py
@@ -5,4 +5,6 @@ import blog.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'index/', blog.views.getIndex),
+    url(r'index/artical_title=(./)', blog.views.getArticalDetail),
 ]
+# handler404 = 'blog.views.page_not_found'
